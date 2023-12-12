b4de11628a2a5a466ba474f29635824c7ddf57dd
MaxRyazan <no4ta87@mail.ru>
2023-12-12 16:17:59 +0300

    1.2.3  - дропдаун

--- src/components/dropdowns/tw-dropdown.vue
+++ src/components/dropdowns/tw-dropdown.vue
@@ -4,8 +4,10 @@ import {ref} from "vue";
 const props = defineProps<{
     modelValue: any
     variants: Array<any>
-    width?: string,
-    borderColor?: string
+    width?: string
+    border?: string
+    largeArrow?: boolean
+    arrowColor?: string
 }>()
 const emit = defineEmits<{
     (e: 'update:modelValue', param: any)
@@ -22,19 +24,44 @@ function chooseVariant(variant: any) {
     <div class="tw_dropdown" :style="{
                 width: width ?? '150px'
            }">
-        <div    :style="{borderBottom: isOpen ? '' : '1px solid red'}"
-                class="tw_dropdown-base"
-                @click="isOpen = !isOpen"
+        <div :style="{
+                borderBottom: isOpen ? '' : (border ? border : '1px solid black'),
+                borderTop: (border ? border : '1px solid black'),
+                borderLeft: (border ? border : '1px solid black'),
+                borderRight: (border ? border : '1px solid black')
+            }"
+             class="tw_dropdown-base"
+             @click="isOpen = !isOpen"
         >
             <div class="tw_dropdown-icon">
-                <div class="tw_dropdown-icon-left"></div>
-                <div class="tw_dropdown-icon-right"></div>
+                <div class="tw_dropdown-icon-left"
+                    :style="{
+                        transform: isOpen ? 'rotate(-35deg)' : 'rotate(35deg)',
+                        borderWidth: largeArrow ? '2px': '1px',
+                        borderColor: arrowColor ?? 'black',
+                        backgroundColor: arrowColor ?? 'black'
+                    }"
+                ></div>
+                <div class="tw_dropdown-icon-right"
+                    :style="{
+                        transform: isOpen ? 'rotate(35deg)' : 'rotate(-35deg)',
+                        borderWidth: largeArrow ? '2px': '1px',
+                        borderColor: arrowColor ?? 'black',
+                        backgroundColor: arrowColor ?? 'black'
+                    }"
+                ></div>
             </div>
             <input readonly type="text" :value="props.modelValue">
         </div>
         <div class="tw_dropdown-variants">
             <transition name="tr_dropdown">
-                <div class="tw_dropdown-variants-item" v-if="isOpen">
+                <div class="tw_dropdown-variants-item"
+                     :style="{
+                        borderLeft: (border ? border : '1px solid black'),
+                        borderRight: (border ? border : '1px solid black'),
+                        borderBottom: (border ? border : '1px solid black'),
+                    }"
+                     v-if="isOpen">
                     <span v-for="variant in props.variants" :key="variant">
                          <span class="tw_variant" v-if="variant !== modelValue"
                                @click="chooseVariant(variant)">{{ variant }}</span>
