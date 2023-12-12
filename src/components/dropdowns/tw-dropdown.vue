82f2a3300ba13bbdf0c22c76c09a58937db7f627
MaxRyazan <no4ta87@mail.ru>
2023-12-12 19:05:43 +0300

    1.2.31  - add hover js

--- src/components/dropdowns/tw-dropdown.vue
+++ src/components/dropdowns/tw-dropdown.vue
@@ -1,23 +1,27 @@
 <script setup lang="ts">
 import {ref, watch} from "vue";
-
+type T = number | string
 const props = defineProps<{
-    modelValue: any
-    variants: Array<any>
+    modelValue: T
+    variants: Array<T>
     width?: string
     border?: string
     largeArrow?: boolean
     arrowColor?: string
     hoverColor?: string
     bgc?: string
+    initialBgc?: string
+    fontSize?:string
+    itemHeight?: string
+    initialHeight?: string
 }>()
 const emit = defineEmits<{
-    (e: 'update:modelValue', param: any)
+    (e: 'update:modelValue', param: T)
 }>()
 const isOpen = ref(false)
 const tw_variant_ref = ref()
 
-function chooseVariant(variant: any) {
+function chooseVariant(variant: T) {
     emit('update:modelValue', variant)
     isOpen.value = false
 }
@@ -26,14 +30,14 @@ watch(tw_variant_ref, () => {
     if (tw_variant_ref.value) {
         if (isOpen.value) {
             tw_variant_ref.value.forEach((item:HTMLElement) => item.addEventListener('mouseover', () => {
-                item.style.backgroundColor = props.hoverColor ? props.hoverColor : 'lightblue'
+                item.style.backgroundColor = props.hoverColor ? props.hoverColor : 'lightgreen'
             }))
             tw_variant_ref.value.forEach((item:HTMLElement) => item.addEventListener('mouseout', () => {
                 item.style.backgroundColor = props.bgc ? props.bgc : 'white'
             }))
         } else {
             tw_variant_ref.value.forEach((item:HTMLElement) => item.removeEventListener('mouseover', () => {
-                item.style.backgroundColor = props.hoverColor ? props.hoverColor : 'lightblue'
+                item.style.backgroundColor = props.hoverColor ? props.hoverColor : 'lightgreen'
             }))
             tw_variant_ref.value.forEach((item:HTMLElement) => item.removeEventListener('mouseout', () => {
                 item.style.backgroundColor = props.bgc ? props.bgc : 'white'
@@ -46,14 +50,17 @@ watch(tw_variant_ref, () => {
 </script>
 
 <template>
-    <div class="tw_dropdown" :style="{
-                width: width ?? '150px'
-           }">
+    <div class="tw_dropdown"
+         :style="{
+            width: width ?? '170px',
+            fontSize: fontSize ?? '14px'
+         }">
         <div :style="{
                 borderBottom: isOpen ? '' : (border ? border : '1px solid black'),
                 borderTop: (border ? border : '1px solid black'),
                 borderLeft: (border ? border : '1px solid black'),
-                borderRight: (border ? border : '1px solid black')
+                borderRight: (border ? border : '1px solid black'),
+                backgroundColor: initialBgc ?? 'white'
             }"
              class="tw_dropdown-base"
              @click="isOpen = !isOpen"
@@ -76,21 +83,37 @@ watch(tw_variant_ref, () => {
                     }"
                 ></div>
             </div>
-            <input readonly type="text" :value="props.modelValue">
+            <input
+                    readonly
+                    type="text"
+                    :value="props.modelValue"
+                    :style="{
+                        backgroundColor: initialBgc ?? 'white',
+                        fontSize: fontSize ?? '14px',
+                        height: initialHeight ? initialHeight : (itemHeight ?? '28px')
+                    }"
+            >
         </div>
         <div class="tw_dropdown-variants">
             <transition name="tr_dropdown">
                 <div class="tw_dropdown-variants-item"
                      v-if="isOpen"
                     :style="{
-                    borderLeft: (border ? border : '1px solid black'),
-                    borderRight: (border ? border : '1px solid black'),
-                    borderBottom: (border ? border : '1px solid black'),
+                        borderLeft: (border ? border : '1px solid black'),
+                        borderRight: (border ? border : '1px solid black'),
+                        borderBottom: (border ? border : '1px solid black'),
+                        backgroundColor: bgc ?? 'white'
                     }"
                 >
                     <span v-for="variant in props.variants" :key="variant">
-                         <span ref="tw_variant_ref" class="tw_variant" v-if="variant !== modelValue"
-                               @click="chooseVariant(variant)">{{ variant }}</span>
+                         <span ref="tw_variant_ref"
+                               class="tw_variant"
+                               v-if="variant !== modelValue"
+                               @click="chooseVariant(variant)"
+                               :style="{
+                                   height: itemHeight ?? '28px'
+                               }"
+                         >{{ variant }}</span>
                     </span>
                 </div>
             </transition>
