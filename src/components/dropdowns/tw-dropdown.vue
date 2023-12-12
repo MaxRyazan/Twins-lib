20e177da2da4826546b07f4ac54a36d14794f4b7
MaxRyazan <no4ta87@mail.ru>
2023-12-12 14:55:49 +0300

    1.2.3  - дропдаун

--- src/components/dropdowns/tw-dropdown.vue
+++ src/components/dropdowns/tw-dropdown.vue
@@ -4,14 +4,15 @@ import {ref} from "vue";
 const props = defineProps<{
     modelValue: any
     variants: Array<any>
-    width?: string
+    width?: string,
+    borderColor?: string
 }>()
 const emit = defineEmits<{
-    (e: 'update:modelValue', param:any)
+    (e: 'update:modelValue', param: any)
 }>()
 const isOpen = ref(false)
 
-function chooseVariant(variant:any){
+function chooseVariant(variant: any) {
     emit('update:modelValue', variant)
     isOpen.value = false
 }
@@ -21,19 +22,25 @@ function chooseVariant(variant:any){
     <div class="tw_dropdown" :style="{
                 width: width ?? '150px'
            }">
-        <div class="tw_dropdown-base" @click="isOpen = !isOpen">
+        <div    :style="{borderBottom: isOpen ? '' : '1px solid red'}"
+                class="tw_dropdown-base"
+                @click="isOpen = !isOpen"
+        >
             <div class="tw_dropdown-icon">
                 <div class="tw_dropdown-icon-left"></div>
                 <div class="tw_dropdown-icon-right"></div>
             </div>
             <input readonly type="text" :value="props.modelValue">
         </div>
-        <transition name="tr_dropdown">
-            <div class="tw_dropdown-variants" v-if="isOpen">
-                <div class="tw_dropdown-variants-item" v-for="variant in props.variants" :key="variant">
-                    <span class="tw_variant" v-if="variant !== modelValue" @click="chooseVariant(variant)">{{variant}}</span>
+        <div class="tw_dropdown-variants">
+            <transition name="tr_dropdown">
+                <div class="tw_dropdown-variants-item" v-if="isOpen">
+                    <span v-for="variant in props.variants" :key="variant">
+                         <span class="tw_variant" v-if="variant !== modelValue"
+                               @click="chooseVariant(variant)">{{ variant }}</span>
+                    </span>
                 </div>
-            </div>
-        </transition>
+            </transition>
+        </div>
     </div>
 </template>
