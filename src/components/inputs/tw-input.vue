be9c13956ad574a86dc73f12ea62c2fa75c72646
MaxRyazan <no4ta87@mail.ru>
2023-12-30 19:08:20 +0300

    1.4.0 tw-input.vue -add variables

--- src/components/inputs/tw-input.vue
+++ src/components/inputs/tw-input.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
+import {onMounted} from "vue";
+
 const props = defineProps<{
     modelValue: any,
 
     width?: string
-    inputWidth?: string
     height?: string
+
+    input_width?: string
+    input_font_size?: string
+    input_font_family?: string
+
     type?: string
     min?: number
     max?: number
     step?: number
-    maxLength?: number
+    max_length?: number
     vertical?: boolean
-    inputTextCenter?: boolean
-    rounded: boolean
-
+    text_align?: string
+    border_radius?: string
+    padding?: string
     placeholder?: string
     disabled?: boolean
     prefix?: string
@@ -21,8 +27,8 @@ const props = defineProps<{
 
     bgc?: string
     border?: string
-    bbColor?: string
-    caretColor?: string
+    border_bottom_color?: string
+    caret_color?: string
     input_color?: string
 
     prefix_color?:string
@@ -43,49 +49,92 @@ function handleAction($event: any){
         emits('update:modelValue', $event.target.value)
     }
 }
+
+onMounted(() => {
+    if (props.input_width) {
+        document.body.style.setProperty('--tw_input_width', props.input_width)
+    }
+    if (props.input_font_family) {
+        document.body.style.setProperty('--tw_input_font_family', props.input_font_family)
+    }
+    if (props.input_font_size) {
+        document.body.style.setProperty('--tw_input_font_size', props.input_font_size)
+    }
+    if (props.prefix_color) {
+        document.body.style.setProperty('--tw_input_prefix_color', props.prefix_color)
+    }
+    if (props.prefix_fz) {
+        document.body.style.setProperty('--tw_input_prefix_font_size', props.prefix_fz)
+    }
+    if (props.prefix_ff) {
+        document.body.style.setProperty('--tw_input_prefix_font_family', props.prefix_ff)
+    }
+    if (props.postfix_color) {
+        document.body.style.setProperty('--tw_input_postfix_color', props.postfix_color)
+    }
+    if (props.postfix_fz) {
+        document.body.style.setProperty('--tw_input_postfix_font_size', props.postfix_fz)
+    }
+    if (props.postfix_ff) {
+        document.body.style.setProperty('--tw_input_postfix_font_family', props.postfix_ff)
+    }
+    if (props.height) {
+        document.body.style.setProperty('--tw_input_height', props.height)
+    }
+    if (props.border) {
+        document.body.style.setProperty('--tw_input_border', props.border)
+    }
+    if (props.border_radius) {
+        document.body.style.setProperty('--tw_input_border_radius', props.border_radius)
+    }
+    if (props.padding) {
+        document.body.style.setProperty('--tw_input_padding', props.padding)
+    }
+    if (props.text_align) {
+        document.body.style.setProperty('--tw_input_text_align', props.text_align)
+    }
+    if (props.bgc) {
+        document.body.style.setProperty('--tw_input_bgc', props.bgc)
+    }
+    if (props.border_bottom_color) {
+        document.body.style.setProperty('--tw_border_bottom_color', props.border_bottom_color)
+    }
+    if (props.caret_color) {
+        document.body.style.setProperty('--tw_input_caret_color', props.caret_color)
+    }
+    if (props.input_color) {
+        document.body.style.setProperty('--tw_input_color', props.input_color)
+    }
+    if (props.width) {
+        document.body.style.setProperty('--tw_input_component_width', props.width)
+    }
+    if (props.height) {
+        document.body.style.setProperty('--tw_input_component_height', props.height)
+    }
+})
 </script>
 
 <template>
-    <div :style="{width: width, height: height}">
-        <div class="tw_wrap" :class="{'tw_vertical':vertical}">
-            <span :style="{
-                      color: prefix_color,
-                      fontSize: prefix_fz,
-                      fontFamily: prefix_ff
-                   }"
-                    v-if="prefix">{{ prefix }}</span>
-            <div :style="{ width: inputWidth}" :class="{'tw_w100': !prefix}" style="gap:10px; display: flex; align-items: center">
+    <div class="tw_input_component">
+        <div class="tw_input_wrap" :class="{'tw_input_vertical':vertical}">
+            <span class="tw_input_prefix" v-if="prefix">{{ prefix }}</span>
+            <div class="tw_input_wrapper" :class="{'tw_input_w100': !prefix}">
                 <input  :value="modelValue"
                         @input="handleAction"
-                        class="tw_input tw_w100"
-                        :style="{
-                            height: height,
-                            border: border,
-                            borderRadius: rounded ? '12px' : '0px',
-                            padding: rounded ? '0 0 0 10px' : '0px',
-                            textAlign: inputTextCenter ? 'center' : 'start',
-                            backgroundColor: bgc,
-                            borderBottomColor: bbColor ? bbColor : (bgc === 'transparent' ? (border ? border : 'white') : bgc),
-                            caretColor: caretColor ? caretColor : (bbColor ? bbColor : (bgc === 'transparent' ? 'black' : bgc)),
-                            color: input_color ?? 'black'
-                         }"
+                        class="tw_input"
+                        :class="{'tw_input_disabled': disabled}"
                         :type="type ?? 'text'"
                         :min="min"
                         :max="max"
                         :step="step"
-                        :maxlength="maxLength"
+                        :maxlength="max_length"
                         :placeholder="placeholder"
                         :disabled="disabled"
                 />
-                <span :style="{
-                            color: postfix_color,
-                            fontSize: postfix_fz,
-                            fontFamily: postfix_ff
-                       }"
-                        v-if="postfix">{{ postfix }}</span>
+                <span  class="tw_input_postfix" v-if="postfix">{{ postfix }}</span>
             </div>
         </div>
-        <span style="margin-top: 2px; color: red;">
+        <span>
             <slot name="validator"/>
         </span>
     </div>
