d829e71122bbecb8a3f6c797e23106a6f8111b5b
MaxRyazan <no4ta87@mail.ru>
2023-12-30 21:39:12 +0300

    1.5.0 tw-button.vue -add variables

--- src/components/buttons/tw-button.vue
+++ src/components/buttons/tw-button.vue
@@ -1,14 +1,6 @@
 <template>
-    <button ref="btn" @click="$emit('push')"
+    <button @click="$emit('push')"
             class="tw_button"
-            :style="{
-                width: width,
-                height: height,
-                color: textColor ? textColor : (color_gamma ? (outline ? color_gamma : 'white') : (outline ? (success ? 'forestgreen': (error ? 'crimson' : (warn ? 'orange' : 'white'))) : 'white')),
-                backgroundColor: bgc ? bgc : color_gamma,
-                border: border,
-                borderColor: color_gamma
-            }"
             :class="{
                 success,
                 warn,
@@ -18,20 +10,18 @@
                 round,
                 rounded,
                 default_hover : color_gamma,
-                default: !color_gamma && !textColor && !success && !error && !warn
+                default: !color_gamma && !text_color && !success && !error && !warn
             }">
         <i class="pi" :class="prime_icon" v-if="prime_icon"></i>
-        <div style="display: flex; align-items: center; justify-content: center; padding: 0">
+        <span class="tw_button_slot">
             <slot/>
-        </div>
+        </span>
     </button>
 </template>
 
 <script setup lang="ts">
 
-import {onMounted, onUnmounted, ref} from "vue";
-
-const btn = ref()
+import {onMounted} from "vue";
 
 const props = defineProps<{
     width?: string,
@@ -46,11 +36,10 @@ const props = defineProps<{
     round?: boolean
     disabled?: boolean,
 
-
     border?: string,
     bgc?: string,
-    bgcHover?: string,
-    textColor?: string,
+    bgc_hover?: string,
+    text_color?: string,
     color_gamma?: string,
 
     prime_icon?: string
@@ -61,25 +50,40 @@ defineEmits<{
 
 
 onMounted(() => {
-    if (props.bgcHover) {
-        btn.value.addEventListener('mouseover', () => {
-            btn.value.style.backgroundColor = props.bgcHover
-        })
-        btn.value.addEventListener('mouseout', () => {
-            btn.value.style.backgroundColor = props.bgc
-        })
+    if (props.width) {
+        document.body.style.setProperty('--tw_button_width', props.width)
+    }
+    if (props.height) {
+        document.body.style.setProperty('--tw_button_height', props.height)
+    }
+    if (props.border) {
+        document.body.style.setProperty('--tw_button_border', props.border)
+    }
+    if (props.outline) {
+        document.body.style.setProperty('--tw_button_bgc', 'transparent')
+    }
+    if (props.bgc_hover) {
+        document.body.style.setProperty('--tw_button_bgc_hover', props.bgc_hover)
+    }
+    if (props.bgc) {
+        document.body.style.setProperty('--tw_button_bgc', props.bgc)
+    }
+    if (props.text_color) {
+        document.body.style.setProperty('--tw_button_text_color', props.text_color)
+    }
+    if(props.color_gamma && props.outline) {
+        document.body.style.setProperty('--tw_button_text_color', props.color_gamma)
+        document.body.style.setProperty('--tw_button_border', props.color_gamma)
+        document.body.style.setProperty('--tw_button_bgc', 'transparent')
+        document.body.style.setProperty('--tw_button_default_hover_color', props.color_gamma)
+    }
+    if(props.color_gamma && !props.outline) {
+        document.body.style.setProperty('--tw_button_text_color', 'white')
+        document.body.style.setProperty('--tw_button_border', props.color_gamma)
+        document.body.style.setProperty('--tw_button_bgc', props.color_gamma)
+        document.body.style.setProperty('--tw_button_default_hover_color', props.color_gamma)
     }
 })
 
-onUnmounted(() => {
-    if (props.bgcHover) {
-        btn.value.removeEventListener('mouseover', () => {
-            btn.value.style.backgroundColor = props.bgcHover
-        })
-        btn.value.removeEventListener('mouseout', () => {
-            btn.value.style.backgroundColor = props.bgc
-        })
-    }
-})
 
 </script>
