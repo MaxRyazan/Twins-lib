85279cdc54f8db20f383b73da903cb8c11ef47af
MaxRyazan <no4ta87@mail.ru>
2024-01-02 19:11:23 +0300

    2.0.2 tw-button.vue

--- src/components/buttons/tw-button.vue
+++ src/components/buttons/tw-button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button @click="$emit('push')"
+    <button ref="tw_lib_Button"
+            @click="$emit('push')"
             :style="{
                 width: width,
                 height: height,
@@ -22,7 +23,9 @@
 </template>
 
 <script setup lang="ts">
-defineProps<{
+import {onMounted, onUnmounted, ref} from "vue";
+
+const props = defineProps<{
     width?: string,
     height?: string,
 
@@ -37,11 +40,35 @@ defineProps<{
     border?: string,
     bgc?: string,
     text_color?: string,
-
+    hover?: string
     prime_icon?: string
 }>()
 defineEmits<{
     (e: 'push'): void
 }>()
+
+const tw_lib_Button = ref()
+
+onMounted(() => {
+    let initialColor = props.bgc ?? document.body.style.getPropertyValue('--tw_button_default_background_color')
+    if(props.success) initialColor = document.body.style.getPropertyValue('--tw_button_success_background_color')
+    if(props.warning) initialColor = document.body.style.getPropertyValue('--tw_button_warning_background_color')
+    if(props.error) initialColor = document.body.style.getPropertyValue('--tw_button_error_background_color')
+    if(props.outline) initialColor = document.body.style.getPropertyValue('--tw_button_outline_background_color')
+    if(props.hover) {
+        tw_lib_Button.value.addEventListener('mouseover', () => {
+            tw_lib_Button.value.style.backgroundColor = props.hover
+        })
+        tw_lib_Button.value.addEventListener('mouseout', () => {
+            tw_lib_Button.value.style.backgroundColor = initialColor
+        })
+    }
+})
+
+onUnmounted(() => {
+    if(props.hover){
+        tw_lib_Button.value.replaceWith(tw_lib_Button.value.cloneNode(true));
+    }
+})
 
 </script>
