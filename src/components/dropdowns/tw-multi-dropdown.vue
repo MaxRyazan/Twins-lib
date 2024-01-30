8aa829e374dfc028f9f8f4cf259ac563b345bb1b
MaxRyazan <no4ta87@mail.ru>
2024-01-30 21:32:42 +0300

    2.2.3 tw-multi-dropdown.vue 100% done

--- src/components/dropdowns/tw-multi-dropdown.vue
+++ src/components/dropdowns/tw-multi-dropdown.vue
@@ -36,49 +36,50 @@ function chooseVariant(variant: T) {
     emit('update:modelValue', variant)
     isOpen.value = false
 }
-function changeVisible(){
+
+function changeVisible() {
     isOpen.value = !isOpen.value
-    if(!isOpen.value) currentSelectedItem.value = null
+    if (!isOpen.value) currentSelectedItem.value = null
 }
 
-function showSubVariants(variant: T){
+function showSubVariants(variant: T) {
     dropdownMultiItemLast.value = null
     currentSelectedItem.value = variant
 }
 
 watch(dropdownMultiItem, () => {
-    if(props.hover_color && dropdownMultiItem.value) {
+    if (props.hover_color && dropdownMultiItem.value) {
         dropdownMultiItem.value.forEach((item: HTMLElement) => item.addEventListener('mouseover', () => {
             item.style.backgroundColor = props.hover_color
         }))
-        dropdownMultiItem.value.forEach((item:HTMLElement) => item.addEventListener('mouseout', () => {
+        dropdownMultiItem.value.forEach((item: HTMLElement) => item.addEventListener('mouseout', () => {
             item.style.backgroundColor = props.bgc ?? ''
         }))
     }
 })
 watch(dropdownMultiItemLast, () => {
-    if(props.hover_color && dropdownMultiItemLast.value) {
+    if (props.hover_color && dropdownMultiItemLast.value) {
         dropdownMultiItemLast.value.forEach((item: HTMLElement) => item.addEventListener('mouseover', () => {
             item.style.backgroundColor = props.hover_color
         }))
-        dropdownMultiItemLast.value.forEach((item:HTMLElement) => item.addEventListener('mouseout', () => {
+        dropdownMultiItemLast.value.forEach((item: HTMLElement) => item.addEventListener('mouseout', () => {
             item.style.backgroundColor = props.bgc ?? ''
         }))
     }
 })
 watch(isOpen, (value) => {
-    if(!value) {
+    if (!value) {
         dropdownMultiItem.value.forEach((item: HTMLElement) => item.removeEventListener('mouseover', () => {
             item.style.backgroundColor = props.hover_color
         }))
-        dropdownMultiItem.value.forEach((item:HTMLElement) => item.removeEventListener('mouseout', () => {
+        dropdownMultiItem.value.forEach((item: HTMLElement) => item.removeEventListener('mouseout', () => {
             item.style.backgroundColor = props.bgc ?? ''
         }))
         dropdownMultiItem.value = null
         dropdownMultiItemLast.value.forEach((item: HTMLElement) => item.removeEventListener('mouseover', () => {
             item.style.backgroundColor = props.hover_color
         }))
-        dropdownMultiItemLast.value.forEach((item:HTMLElement) => item.removeEventListener('mouseout', () => {
+        dropdownMultiItemLast.value.forEach((item: HTMLElement) => item.removeEventListener('mouseout', () => {
             item.style.backgroundColor = props.bgc ?? ''
         }))
         dropdownMultiItemLast.value = null
@@ -134,36 +135,57 @@ watch(isOpen, (value) => {
         >
             <div v-for="variant in variants"
                  :key="Object.keys(variant)[0]"
-                :style="{ backgroundColor: bgc }">
-                <div v-for="obj in Object.values(variant)" :key="Object.keys(variant)[0]" class="tw_dropdown_multi__first-title">
-                    <div style="width: 100%; cursor:pointer;"
-                         @mouseover="showSubVariants(Object.keys(variant)[0])">
-                        <span ref="dropdownMultiItem"
-                              style="width: 100%; height: 100%;"
-                              :style="{
-                                  color: color,
-                                  height: items_height,
-                                  padding: padding,
-                                  paddingRight: '35px',
-                                  justifyContent: text_center ? 'center' : 'start',
-                                  fontSize: font_size,
-                                  fontFamily: font_family,
-                                  fontWeight: font_weight
-                              }"
-                        >
-                            {{Object.keys(variant)[0]}}
-                        </span>
-                    </div>
-                    <div class="tw_dropdown_multi__list"
-                         v-if="currentSelectedItem === Object.keys(variant)[0]">
-                        <ul class="tw_dropdown_multi_last_title"
+                 :style="{ backgroundColor: bgc }">
+                <div  class="tw_dropdown_multi__first-title"
+                      v-if="typeof variant !== 'object' && (typeof variant === 'string' || typeof variant === 'number')">
+                    <span @mouseover="currentSelectedItem = null"
+                           ref="dropdownMultiItem"
+                           style="width: 100%; height: 100%;"
+                           :style="{
+                               color: color,
+                               height: items_height,
+                               padding: padding,
+                               paddingRight: '35px',
+                               justifyContent: text_center ? 'center' : 'start',
+                               fontSize: font_size,
+                               fontFamily: font_family,
+                               fontWeight: font_weight
+                           }"
+                    >
+                          {{ variant }}
+                    </span>
+                </div>
+                <div v-else>
+                    <div v-for="obj in Object.values(variant)" :key="Object.keys(variant)[0]"
+                         class="tw_dropdown_multi__first-title">
+                        <div style="width: 100%; cursor:pointer;"
+                             @mouseover="showSubVariants(Object.keys(variant)[0])">
+                            <span ref="dropdownMultiItem"
+                                  style="width: 100%; height: 100%;"
+                                  :style="{
+                                      color: color,
+                                      height: items_height,
+                                      padding: padding,
+                                      paddingRight: '35px',
+                                      justifyContent: text_center ? 'center' : 'start',
+                                      fontSize: font_size,
+                                      fontFamily: font_family,
+                                      fontWeight: font_weight
+                                  }"
+                            >
+                                {{ Object.keys(variant)[0] }}
+                            </span>
+                        </div>
+                        <div class="tw_dropdown_multi__list"
+                             v-if="currentSelectedItem === Object.keys(variant)[0]">
+                            <ul class="tw_dropdown_multi_last_title"
                                 :style="{
                                      border: border,
                                        backgroundColor: bgc
                             }">
-                            <li ref="dropdownMultiItemLast"
-                                v-for="item in obj" :key="item"
-                                :style="{
+                                <li ref="dropdownMultiItemLast"
+                                    v-for="item in obj" :key="item"
+                                    :style="{
                                     height: items_height,
                                     padding: padding,
                                     color: color,
@@ -172,8 +194,10 @@ watch(isOpen, (value) => {
                                     fontFamily: font_family,
                                     fontWeight: font_weight
                                 }"
-                                @click="chooseVariant(item)">{{item}}</li>
-                        </ul>
+                                    @click="chooseVariant(item)">{{ item }}
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
