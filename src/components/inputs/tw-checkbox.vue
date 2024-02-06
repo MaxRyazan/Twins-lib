7d956eab79b525164184074d204efd72684e900c
MaxRyazan <no4ta87@mail.ru>
2024-02-06 20:24:47 +0300

    2.3.0 tw-checkbox.vue

---
+++ src/components/inputs/tw-checkbox.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const inner = ref()
+const props = defineProps<{
+    label?: string
+}>()
+
+function check() {
+    inner.value.classList.toggle('tw_checkbox-hidden')
+}
+</script>
+
+<template>
+    <div class="tw_checkbox__wrapper"
+         contenteditable="true"
+         @keydown.enter.prevent="check"
+         @click="check"
+    >
+        <span ref="inner" class="tw_checkbox__inner tw_checkbox-hidden"></span>
+    </div>
+</template>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import TwTableBase from "@/components/tables/tw-table-base.vue";
 import TwInput from "@/components/inputs/tw-input.vue";
 import TwDropdown from "@/components/dropdowns/tw-dropdown.vue";
 import TwMultiDropdown from "@/components/dropdowns/tw-multi-dropdown.vue";
+import TwCheckbox from "@/components/inputs/tw-checkbox.vue";
 export default {
     install: (app) => {
         app.component("tw-button", TwButton);
@@ -10,7 +11,8 @@ export default {
         app.component("tw-input", TwInput);
         app.component("tw-dropdown", TwDropdown);
         app.component("tw-multi-dropdown", TwMultiDropdown);
+        app.component("tw-checkbox", TwCheckbox);
     }
 }
 
-export {TwInput, TwButton, TwTableBase, TwDropdown, TwMultiDropdown}
+export {TwInput, TwButton, TwTableBase, TwDropdown, TwMultiDropdown, TwCheckbox}
