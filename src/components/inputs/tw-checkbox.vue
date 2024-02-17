d604602436aacdd073e9159ce37e8eae7dc8c5b9
MaxRyazan <no4ta87@mail.ru>
2024-02-17 21:16:38 +0300

    2.3.3 tw-checkbox.vue

--- src/components/inputs/tw-checkbox.vue
+++ src/components/inputs/tw-checkbox.vue
@@ -8,6 +8,7 @@ const emits = defineEmits<{
 
 let uuid = ref("")
 let twCheckbox = ref()
+let twInnerWrapper = ref()
 
 const props = defineProps<{
     modelValue: boolean
@@ -23,29 +24,44 @@ function check() {
     if(props.modelValue) {
         emits('update:modelValue', false)
         twCheckbox.value.checked = false
-            console.log(window.getComputedStyle(
-        document.querySelector(`#${uuid.value}`), ':before'
-    ))
+        twInnerWrapper.value.classList.add('tw_checkbox__hide')
     }
     else {
         emits('update:modelValue', true)
         twCheckbox.value.checked = true
-            console.log(window.getComputedStyle(
-        document.querySelector(`#${uuid.value}`), ':before'
-    ))
+        twInnerWrapper.value.classList.remove('tw_checkbox__hide')
     }
 }
 onMounted(() => {
     uuid.value = generateUUID()
-    if(props.modelValue) twCheckbox.value.checked = true
+    if(props.modelValue) {
+        twCheckbox.value.checked = true
+        twInnerWrapper.value.classList.remove('tw_checkbox__hide')
+    }
 
 })
 </script>
 
 <template>
-    <div class="tw_checkbox__wrapper">
-        <input ref="twCheckbox" @click="check" type="checkbox" :id="uuid">
-        <label :for="uuid"></label>
+    <div class="tw_checkbox__wrapper" :style="{gap: gap}">
+        <div @click="check"
+             class="tw_checkbox__inner-wrapper"
+             :style="{
+                border: border,
+                width: size,
+                height: size,
+                backgroundColor: bgc,
+             }"
+        >
+            <div ref="twInnerWrapper"
+                 class="tw_checkbox__check-symbol tw_checkbox__hide"
+                 :style="{
+                    border: `2px solid ${checkedColor}`,
+                    borderWidth: '0 2px 2px 0'
+                 }"
+            ></div>
+            <input ref="twCheckbox" type="checkbox" :id="uuid">
+        </div>
         <span @click="check" class="tw_checkbox__text">{{label}}</span>
     </div>
 </template>
